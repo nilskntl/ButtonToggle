5c2081170f39593e520b87f5c30970791369bacf
Nils Knittel <go56bog@mytum.de>
2023-12-23 17:16:24 +0100

    Added comments

--- ButtonToggle.java
+++ ButtonToggle.java
@@ -14,90 +14,83 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class ButtonToggle extends GuiButton {
 
-	Color off = new Color (0xFFbdbdbd); // Off color
-	Color on = new Color (0xFF00FF00); // On Color
-	Color c = new Color (0xFFFFFFFF); // Circle Color
-	
-    public ButtonToggle(int i, int j, int k, String s)
-    {
-    	this(i, j, k, 120, 20, s);
+    Color off = new Color(0xFFbdbdbd); // Off color
+    Color on = new Color(0xFF00FF00);  // On Color
+    Color c = new Color(0xFFFFFFFF);   // Circle Color
+
+    public ButtonToggle(int i, int j, int k, String s) {
+        this(i, j, k, 120, 20, s);
     }
 
-    public ButtonToggle(int i, int j, int k, int l, int i1, String s)
-    {
+    public ButtonToggle(int i, int j, int k, int l, int i1, String s) {
         super(i, j, k, l, i1, s);
         this.enabled = true;
         this.visible = true;
     }
 
-    protected int getHoverState(boolean flag)
-    {
-    	byte byte0 = 1;
-    	if (!enabled)
-    	{
-    		byte0 = 0;
-    		}
-    	else if (flag)
-    	{
-    		byte0 = 2;
-    	}
-    	return byte0;
+    protected int getHoverState(boolean flag) {
+        byte byte0 = 1;
+        if (!enabled) {
+            byte0 = 0;
+        } else if (flag) {
+            byte0 = 2;
+        }
+        return byte0;
     }
-    
+
     // Draw Button
-    public void drawButton(Minecraft mc, int mx, int my)
-    {
-    	// If you want to use a hover animation
-    	// boolean flag = mx >= xPosition && my >= yPosition && mx < xPosition + width && my < yPosition + height;
-    	
-    	GlStateManager.color(1, 1, 1);
-    	if(load(this.displayString) == true) {
-    		drawRoundedRect(xPosition, yPosition, width, height, height / 2, on);
-    		drawCircle(xPosition + width - height + 1, yPosition + 1, height - 2, height - 2, c);
-    	} else {
-    		drawRoundedRect(xPosition, yPosition, width, height, height / 2, off);
-    	    drawCircle(xPosition + 1, yPosition + 1, height - 2, height - 2, c);
-    	}
-    	
+    public void drawButton(Minecraft mc, int mx, int my) {
+        // If you want to use a hover animation
+        // boolean flag = mx >= xPosition && my >= yPosition && mx < xPosition + width && my < yPosition + height;
+
+        GlStateManager.color(1, 1, 1);
+        if (load(this.displayString)) {
+            drawRoundedRect(xPosition, yPosition, width, height, height / 2, on);
+            drawCircle(xPosition + width - height + 1, yPosition + 1, height - 2, height - 2, c);
+        } else {
+            drawRoundedRect(xPosition, yPosition, width, height, height / 2, off);
+            drawCircle(xPosition + 1, yPosition + 1, height - 2, height - 2, c);
+        }
     }
-    
+
     // Toggle
     public static File getFolder(String toggle) {
-		File file = new File(FileManager.TOGGLE_DIR, toggle);
-		file.mkdirs();
-		return file;
-	}
-	
+        File file = new File(FileManager.TOGGLE_DIR, toggle);
+        file.mkdirs();
+        return file;
+    }
+
     public static void save(String toggle, Boolean b) {
-		FileManager.writeJsonToFile(new File(getFolder(toggle), "Toggle.json"), b);
-	}
+        FileManager.writeJsonToFile(new File(getFolder(toggle), "Toggle.json"), b);
+    }
+
     public static Boolean load(String toggle) {
-		Boolean b = FileManager.readFromJson(new File(getFolder(toggle), "Toggle.json"), Boolean.class);
-		
-		if (b == null) {
-			b = false;
-			save(toggle, b);
-		}
-		return b;
-	}
-    
+        Boolean b = FileManager.readFromJson(new File(getFolder(toggle), "Toggle.json"), Boolean.class);
+
+        if (b == null) {
+            b = false;
+            save(toggle, b);
+        }
+        return b;
+    }
+
     private void drawRoundedRect(int x, int y, int width, int height, int cornerRadius, Color color) {
-    	this.drawRect(x, y + cornerRadius, x + cornerRadius, y + height - cornerRadius, color.getRGB());
-    	this.drawRect(x + cornerRadius, y, x + width - cornerRadius, y + height, color.getRGB());
-    	this.drawRect(x + width - cornerRadius, y + cornerRadius, x + width, y + height - cornerRadius, color.getRGB());
+        this.drawRect(x, y + cornerRadius, x + cornerRadius, y + height - cornerRadius, color.getRGB());
+        this.drawRect(x + cornerRadius, y, x + width - cornerRadius, y + height, color.getRGB());
+        this.drawRect(x + width - cornerRadius, y + cornerRadius, x + width, y + height - cornerRadius, color.getRGB());
         drawArc(x + cornerRadius, y + cornerRadius, cornerRadius, 0, 90, color);
         drawArc(x + width - cornerRadius, y + cornerRadius, cornerRadius, 270, 360, color);
         drawArc(x + width - cornerRadius, y + height - cornerRadius, cornerRadius, 180, 270, color);
         drawArc(x + cornerRadius, y + height - cornerRadius, cornerRadius, 90, 180, color);
     }
-    
+
     private void drawArc(int x, int y, int radius, int startAngle, int endAngle, Color color) {
         GL11.glPushMatrix();
         GL11.glEnable(3042);
         GL11.glDisable(3553);
         GL11.glBlendFunc(770, 771);
         GL11.glColor4f((float) color.getRed() / 255, (float) color.getGreen() / 255, (float) color.getBlue() / 255, (float) color.getAlpha() / 255);
-        
+
         WorldRenderer worldRenderer = Tessellator.getInstance().getWorldRenderer();
 
         worldRenderer.begin(6, DefaultVertexFormats.POSITION);
@@ -107,16 +100,15 @@ public class ButtonToggle extends GuiButton {
             double angle = (Math.PI * 2 * i / 100) + Math.toRadians(180);
             worldRenderer.pos(x + Math.sin(angle) * radius, y + Math.cos(angle) * radius, 0).endVertex();
         }
-        
+
         Tessellator.getInstance().draw();
-        
+
         GL11.glEnable(3553);
         GL11.glDisable(3042);
         GL11.glPopMatrix();
     }
-    
+
     private void drawCircle(int x, int y, int width, int height, Color color) {
-    	this.drawArc(x + width / 2, y + height / 2, width / 2, 0, 360, color);
+        this.drawArc(x + width / 2, y + height / 2, width / 2, 0, 360, color);
     }
-    
 }
--- FileManager.java
+++ FileManager.java
@@ -10,65 +10,88 @@ import java.io.InputStreamReader;
 import com.google.gson.Gson;
 
 public class FileManager {
-	
+
 	private static Gson gson = new Gson();
-	
+
+	// Define the root directory and the toggle directory within it
 	private static File ROOT_DIR = new File("client");
 	public static File TOGGLE_DIR = new File(ROOT_DIR, "Toggle");
-	
+
+	// Initialize the file manager by creating necessary directories if they do not exist
 	public static void init() {
 		if(!ROOT_DIR.exists()) { ROOT_DIR.mkdirs(); }
 		if(!TOGGLE_DIR.exists()) { TOGGLE_DIR.mkdirs(); }
 	}
-	
+
+	// Getter method to retrieve the Gson instance used for JSON serialization/deserialization
 	public static Gson getGson() {
 		return gson;
 	}
 
+	// Write an object to a JSON file
 	public static boolean writeJsonToFile(File file, Object obj) {
-		
+
 		try {
+			// If the file does not exist, create it
 			if (!file.exists()) {
 				file.createNewFile();
 			}
-			
+
+			// Open a FileOutputStream for the specified file
 			FileOutputStream outputStream = new FileOutputStream(file);
+
+			// Write the JSON representation of the object to the file
 			outputStream.write(gson.toJson(obj).getBytes());
+
+			// Flush and close the output stream
 			outputStream.flush();
 			outputStream.close();
+
+			// Indicate that the operation was successful
 			return true;
 		}
 		catch (IOException e) {
+			// Print the stack trace if an exception occurs during the operation
 			e.printStackTrace();
+			// Indicate that the operation failed
 			return false;
 		}
-		
+
 	}
-	
+
+	// Read an object from a JSON file
 	public static <T extends Object> T readFromJson(File file, Class<T> c) {
-		
+
 		try {
-			
+			// Open a FileInputStream for the specified file
 			FileInputStream fileInputStream = new FileInputStream(file);
+
+			// Create InputStreamReader and BufferedReader for efficient reading
 			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
 			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-			
+
+			// Read the contents of the file into a StringBuilder
 			StringBuilder builder = new StringBuilder();
 			String line;
-			
+
+			// Append each line of the file to the StringBuilder
 			while((line = bufferedReader.readLine()) != null) {
 				builder.append(line);
 			}
-			
+
+			// Close the BufferedReader, InputStreamReader, and FileInputStream
 			bufferedReader.close();
 			inputStreamReader.close();
 			fileInputStream.close();
-			
+
+			// Convert the JSON string to the specified object type using Gson
 			return gson.fromJson(builder.toString(), c);
-			
+
 		}
 		catch (IOException e) {
+			// Print the stack trace if an exception occurs during the operation
 			e.printStackTrace();
+			// Return null to indicate failure
 			return null;
 		}
 	}
